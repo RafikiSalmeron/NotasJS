23bf6927bd6b62d299c6bcc55dfdef11c78bcba1
RafikiSalmeron <rafikisalmeronmartos@gmail.com>
2020-12-13 18:19:27 +0100

    Primer Commit
    No hay MVC aun

---
+++ Modelo.js
@@ -0,0 +1,10 @@
+// Clase Nota
+export class Nota{
+	constructor(id, titulo, texto, fecha){
+		// Declaración de variables
+		this.id = id;
+		this.titulo = titulo;
+		this.texto = texto;
+		this.fecha = fecha;
+	}
+}
